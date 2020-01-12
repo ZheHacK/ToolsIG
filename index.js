faa7dc46e9ae07b523460c4567870b3eee1588b5
ZhehacK <dhenhabsyi55@gmail.com>
2020-01-13 02:13:53 +0700

    /data/data/com.termux/files/home/ToolsIG

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const Client = require('instagram-private-api').V1;
+const delay = require('delay');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+
+const questionTools = [
+  {
+    type:"list",
+    name:"Tools",
+    message:"Pilih tools:",
+    choices:
+      [
+        "[=>]  Follow Followers Target ~ with Comment",
+        "[=>]  Follow Followers Target ~ with DM",
+        "[=>]  Follow Followers Target ~ No Like",
+        "[=>]  Unfollow Not Followback",
+        ""
+      ] 
+  }
+]
+
+const main = async () => {
+  var spinner;
+  try{
+    var toolChoise = await inquirer.prompt(questionTools);
+    toolChoise = toolChoise.Tools;
+    switch(toolChoise){
+      case "[=>]  Follow Followers Target ~ with Comment":
+        await require("./tools/FFT.js");
+        break;
+      case "[=>]  Follow Followers Target ~ with DM":
+        await require("./tools/FFTDM.js");
+        break;
+      case "[=>]  Follow Followers Target ~ No Like":
+        await require("./tools/FFTnoLike.js");
+        break;
+      case "[=>]  Unfollow Not Followback":
+        await require("./tools/unfollowNotFollowBack.js")
+        break;
+      default:
+        console.log('\nERROR:\n[?] Aw, Snap! \n[!] Ada yang salah saat menampilkan program ini! \n [!] Silakan coba lagi!');
+    }
+  } catch(e) {
+    //spinner.stop(true);
+    //console.log(e);
+    //ZheAlHaqy
+  }
+}
+
+console.log(chalk`
+  {red.yellow
+  ╔╦╗┌─┐┌─┐┬  ┌─┐    © 2019
+   ║ │ ││ ││  └─┐   CyberZhe 
+   ╩ └─┘└─┘┴─┘└─┘   ---------     
+{bold.green
+Blog   : zhealhaqy.blogspot.com
+Github : github.com/ZheHacK
+Insta  : instagram.com/zhe_alhaqy
+___________________________________}
+}
+      `);
+
+main()
